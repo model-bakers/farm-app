d28ef201d8ee7f85ab7039b42736148a3c39065f
Ana Paula Gomes <apgomes88@gmail.com>
2021-01-08 17:49:13 +0100

    Fix versions

---
+++ farms/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Thing(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    instrument = models.CharField(max_length=100)
+
+class Album(models.Model):
+    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    num_stars = models.IntegerField()
+
+    # TODO add property
