01782b9c6ac0a8161b7ae9a4bb971b50cf9f1d57
Ana Paula Gomes <1899950+anapaulagomes@users.noreply.github.com>
2021-01-18 13:16:42 +0100

    Add OneToOne relation (#11)
    * Add OneToOne example
    
    * Add _quantity example
    
    * Add test with _fill_optional=True and FK lookup

--- farms/tests/test_models.py
+++ farms/tests/test_models.py
@@ -8,8 +8,13 @@ class TestFarm:
         # one way of importing a model: app_label.model_name
         farm = baker.prepare('farms.Farm')  # won't be persisted
         assert isinstance(farm.name, str)
-        assert isinstance(farm.address, str)
         assert farm.area is not None
+
+    def test_add_address_to_a_farm(self):
+        # an example of OneToOne relation
+        farm = baker.prepare('farms.Farm', _fill_optional=["address"])
+        # returns a persisted address (see our docs)
+        assert farm.address
 
 
 class TestFarmer:
@@ -32,3 +37,23 @@ class TestAnimal:
         # another way: directly importing
         cow = baker.make(Animal, specie=Animal.Species.COW)  # using make will persist the object
         assert cow.specie == "COW"
+
+
+@pytest.mark.django_db  # needed because we are accessing the db with make
+class TestAddress:
+    def test_create_addresses(self):
+        addresses = baker.make('farms.Address', _quantity=5)
+
+        assert len(addresses) == 5
+
+    def test_create_address_with_optional_fields_filed(self):
+        # this is a nice shortcut if you want all optional fields to be filed
+        address = baker.make('farms.Address', _fill_optional=True)
+
+        assert address.additional_info is not None
+
+    def test_brazilian_farm(self):
+        # creates an Address object random values for all fields, but country
+        farm = baker.prepare('farms.Farm', address__country='Brazil')
+        assert farm.address
+        assert farm.address.country == 'Brazil'
